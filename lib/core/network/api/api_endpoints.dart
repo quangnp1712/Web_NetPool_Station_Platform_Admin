4bd2a885415c854134c99fc5ca92698c7c65e961
quangnp1712 <quangnp1712@gmail.com>
2025-12-26 12:26:48 +0700

    update code

--- lib/core/network/api/api_endpoints.dart
+++ lib/core/network/api/api_endpoints.dart
@@ -1,5 +1,6 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-String domainUrl = "${dotenv.env["DOMAIN"]}";
+String domainUrl = String.fromEnvironment('DOMAIN',
+    defaultValue:
+        'https://nps-core-service-6b8b355ec2a3.herokuapp.com' // Link dự phòng chạy local
+    );
 
 class ApiEndpoints {}
